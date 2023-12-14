3dcaad14dfb29b56147dea469988dd729aa860ca
Josh Mock <joshua.mock@elastic.co>
2023-12-14 11:02:10 -0600

    Reintroduce skips to integration tests

--- test/integration/index.js
+++ test/integration/index.js
@@ -45,6 +45,35 @@ const options = minimist(process.argv.slice(2), {
   string: ['suite', 'test'],
 })
 
+const skips = {
+  // TODO: sql.getAsync does not set a content-type header but ES expects one
+  // transport only sets a content-type if the body is not empty
+  'sql/10_basic.yml': ['*'],
+  // TODO: bulk call in setup fails due to "malformed action/metadata line"
+  // bulk body is being sent as a Buffer, unsure if related.
+  'transform/10_basic.yml': ['*'],
+  // TODO: scripts_painless_execute expects {"result":"0.1"}, gets {"result":"0"}
+  // body sent as Buffer, unsure if related
+  'script/10_basic.yml': ['*']
+}
+
+const shouldSkip = (file, name) => {
+  if (options.suite || options.test) return false
+
+  let keys = Object.keys(skips)
+  for (let key of keys) {
+    if (key.endsWith(file) || file.endsWith(key)) {
+      const tests = skips[key]
+      if (tests.includes('*') || tests.includes(name)) {
+        log(`Skipping test "${file}: ${name}" because it is on the skip list`)
+        return true
+      }
+    }
+  }
+
+  return false
+}
+
 const getAllFiles = async dir => {
   const files = await globby(dir, {
     expandDirectories: {
@@ -138,6 +167,12 @@ async function start ({ client }) {
       const junitTestCase = junitTestSuite.testcase(name, `node_${process.version}: ${cleanPath}`)
 
       stats.total += 1
+      if (shouldSkip(file, name)) {
+        stats.skip += 1
+        junitTestCase.skip('This test is on the skip list')
+        junitTestCase.end()
+        continue
+      }
       log('  - ' + name)
       try {
         await testRunner.run(setupTest, test[name], teardownTest, stats, junitTestCase)
